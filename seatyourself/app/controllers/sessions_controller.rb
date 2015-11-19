6e4522e49be1d34c6e1ceda5aa9f1419681425b7
margaretyjkim <margaret.yj.kim@gmail.com>
2015-11-19 14:43:42 -0500

    ensure login and log out

--- seatyourself/app/controllers/sessions_controller.rb
+++ seatyourself/app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   	user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to products_url, notice: "Logged in!"
+      redirect_to restaurants_url, notice: "Logged in!"
     else
       render "new"
     end
@@ -16,5 +16,5 @@ class SessionsController < ApplicationController
   	session[:user_id] = nil
     redirect_to products_url, notice: "Logged out!"
   end
-  end
 end
+
